b0bacd87ed550d7bb109027e51bcf50e780d667a
renhaoran <RHRxjtu@outlook.com>
2017-11-28 00:42:02 +0800

    my boll tendency

---
+++ BollMiddleSlope.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using QuantTC;
+using QuantTC.Data;
+using QuantTC.Indicators.Generic;
+
+namespace QuantIX.BOLL
+{
+    public class BollMiddleSlope : Indicator<double>
+    {
+        public BollMiddleSlope(IIndicator<double> source)
+        {
+            Source = source;
+            Source.Update += SourceOnUpdate;
+        }
+
+        private void SourceOnUpdate()
+        {
+            Data.FillRange(Count, Source.Count, i => i > 0 ? Source[i] - Source[i - 1] : Source[0]);
+            FollowUp();
+        }
+
+        public IIndicator<double> Source { get; }
+    }
+}
---
+++ BollTendency.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using QuantTC;
+using QuantTC.Indicators.Generic;
+//   上升或者下降的状态都可以用当前值与前一个值的差来判断，来快速判断当前的状态是上升还是下降 当然有一个大小，大于某一个值的时候是上升
+namespace QuantIX.BOLL
+{
+    public class BollTendency: Indicator<BollTendency.BOLLDatum>
+    {
+        public BollTendency(IIndicator<double> source, double bollMiddlePlainEdge)
+        {
+            Source = source;
+            BOLLMiddlePlainEdge = bollMiddlePlainEdge;
+            Source.Update += SourceOnUpdate;
+        }
+
+        private void SourceOnUpdate()
+        {
+            Data.FillRange(Count, Source.Count, i =>
+            {
+                if (i < 3)
+                {
+                    if (Source[i].Abs() < BOLLMiddlePlainEdge)
+                    {
+                        return BOLLDatum.Plain;
+                    }
+                    if (Source[i] > 0)
+                    {
+                        return BOLLDatum.Rise;
+                    }
+                    return BOLLDatum.Decline;
+                }
+                if ((Source[i] + Source[i - 1] + Source[i - 2])  / 2 < BOLLMiddlePlainEdge)
+                {
+                    return BOLLDatum.Plain;
+                }
+                if (Source[i] < 0)
+                {
+                    return BOLLDatum.Decline;
+                }
+                return BOLLDatum.Rise;
+            });
+            FollowUp();
+        }
+
+        public enum BOLLDatum
+        {
+            Plain, Rise, Decline
+        }
+
+        public double BOLLMiddlePlainEdge { get; set; }
+
+        public IIndicator<double> Source { get; }
+    }
+    
+}
